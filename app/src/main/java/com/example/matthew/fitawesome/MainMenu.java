1aa39627f24e82263492799c3be41d87fc9cdfdd
mtcattin <mtcattin@gmail.com>
2016-10-24 15:02:49 -0600

    Revised to be API 6.0 use this!!!

--- app/src/main/java/com/example/matthew/fitawesome/MainMenu.java
+++ app/src/main/java/com/example/matthew/fitawesome/MainMenu.java
@@ -26,7 +26,9 @@ public class MainMenu extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+    } System.out.println("Hey guys!!!");
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
