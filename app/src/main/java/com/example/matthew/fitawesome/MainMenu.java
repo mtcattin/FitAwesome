4cac9ce2880937081e58a824db3da4b7d78d78fd
sarah <scattin000@gmail.com>
2016-10-26 09:38:25 -0600

    added spaces :)

--- app/src/main/java/com/example/matthew/fitawesome/MainMenu.java
+++ app/src/main/java/com/example/matthew/fitawesome/MainMenu.java
@@ -17,7 +17,7 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+     
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
