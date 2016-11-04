fd93d63840606a8f8a0dfefbb76deba5285d76e8
mtcattin <mtcattin@gmail.com>
2016-11-04 13:36:17 -0600

    Added activities for day, week number and exercise log

---
+++ app/src/main/java/com/example/matthew/fitawesome/Day.java
@@ -0,0 +1,13 @@
+package com.example.matthew.fitawesome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Day extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
