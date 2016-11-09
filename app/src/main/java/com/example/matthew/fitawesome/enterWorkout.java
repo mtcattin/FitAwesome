92baf5fe7acc911e0987071dabb5a0097ed623e1
sarah <scattin000@gmail.com>
2016-11-09 14:04:25 -0700

    added variables to the enter workout activity

--- app/src/main/java/com/example/matthew/fitawesome/enterWorkout.java
+++ app/src/main/java/com/example/matthew/fitawesome/enterWorkout.java
@@ -1,13 +1,23 @@
 package com.example.matthew.fitawesome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.widget.Button;
 
 public class enterWorkout extends AppCompatActivity {
+
+    private Button createNewEx;
+    private Button viewWorkout;
+    private SearchView exerciseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_workout);
+
+        createNewEx = (Button) findViewById(R.id.createNewEx_btn);
+        viewWorkout = (Button) findViewById(R.id.viewWout_btn);
+        exerciseDatabase = (SearchView) findViewById(R.id.srchExData);
     }
 }
