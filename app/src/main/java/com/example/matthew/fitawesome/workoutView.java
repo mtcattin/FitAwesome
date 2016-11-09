bfee44beb88a6f1781c4f91ef64f7e7121504021
sarah <scattin000@gmail.com>
2016-11-09 14:00:55 -0700

    added variables to the workout view activity

--- app/src/main/java/com/example/matthew/fitawesome/workoutView.java
+++ app/src/main/java/com/example/matthew/fitawesome/workoutView.java
@@ -1,13 +1,23 @@
 package com.example.matthew.fitawesome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ExpandableListView;
 
 public class workoutView extends AppCompatActivity {
+
+    private Button completeWorkout;
+    private Button clearWorkout;
+    private ExpandableListView workoutDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_view);
+
+        completeWorkout = (Button) findViewById(R.id.completeWout_btn);
+        clearWorkout = (Button) findViewById(R.id.clrWout_btn);
+        workoutDisplay = (ExpandableListView) findViewById(R.id.list_woutContents);
     }
 }
