83834e7fe14552a5023344af1c42e70115bd9596
sarah <scattin000@gmail.com>
2016-11-11 14:38:21 -0700

    updated both the enterWorkout and the weekday... have a few questions though on those

--- app/src/main/java/com/example/matthew/fitawesome/WeekDay.java
+++ app/src/main/java/com/example/matthew/fitawesome/WeekDay.java
@@ -2,9 +2,11 @@ package com.example.matthew.fitawesome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class WeekDay extends AppCompatActivity {
+public class WeekDay extends AppCompatActivity implements OnClickListener {
 
     private Button sunday;
     private Button monday;
@@ -19,7 +21,7 @@ public class WeekDay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_day);
 
-
+        // link the buttons to the variables
         sunday = (Button) findViewById(R.id.sun_btn);
         monday = (Button) findViewById(R.id.mon_btn);
         tuesday = (Button) findViewById(R.id.tues_btn);
@@ -27,5 +29,27 @@ public class WeekDay extends AppCompatActivity {
         thursday = (Button) findViewById(R.id.thurs_btn);
         friday = (Button) findViewById(R.id.fri_btn);
         saturday = (Button) findViewById(R.id.sat_btn);
+
+        // set up the buttons for activity
+        sunday.setOnClickListener(this);
+        monday.setOnClickListener(this);
+        tuesday.setOnClickListener(this);
+        wednesday.setOnClickListener(this);
+        thursday.setOnClickListener(this);
+        friday.setOnClickListener(this);
+        saturday.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if((view == sunday) || (view == monday) || (view == tuesday) || (view == wednesday)
+                || (view == thursday) || (view == friday) || (view == saturday)) {
+            // open up to enter workout page
+            // specify where the data will be stored??
+            /*should all the days be listed in this single if statement? since they
+             all go to the same page? Or should they each have thier own set up?
+             see wk 10 agenda for more Q's and details...
+             */
+        }
     }
 }
--- app/src/main/java/com/example/matthew/fitawesome/enterWorkout.java
+++ app/src/main/java/com/example/matthew/fitawesome/enterWorkout.java
@@ -3,9 +3,11 @@ package com.example.matthew.fitawesome;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class enterWorkout extends AppCompatActivity {
+public class enterWorkout extends AppCompatActivity implements OnClickListener {
 
     private Button createNewEx;
     private Button viewWorkout;
@@ -19,5 +21,19 @@ public class enterWorkout extends AppCompatActivity {
         createNewEx = (Button) findViewById(R.id.createNewEx_btn);
         viewWorkout = (Button) findViewById(R.id.btn_week2);
         exerciseDatabase = (SearchView) findViewById(R.id.srchExData);
+
+        // need to figure out how to set up the search still
+        createNewEx.setOnClickListener(this);
+        viewWorkout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == createNewEx) {
+            // take to the exerciseDetails activity
+        }
+        if(view == viewWorkout) {
+            // taje to the workout view Activity
+        }
     }
 }
